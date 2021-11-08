8ec58e72278fd5ade037d863a90454f7eaf845fa
Yago <nietogarridoyago@gmail.com>
2021-11-08 13:47:54 +0100

    3333.01 nombre y numero de matricula tienen valor aunque haya error como quiere el  profe

--- Alumno.java
+++ Alumno.java
@@ -1,5 +1,5 @@
 public class Alumno {
-    
+
     // el nombre completo del alumno
     private String nombre;
     // el numero de matricula
@@ -13,14 +13,14 @@ public class Alumno {
     public Alumno(String nombreCompleto, String numeroMatriculaAlumno, int edadAlumno) {
         if(nombreCompleto.length() < 3){
             System.out.println("!ERROR¡ Nombre muy corto");
-            nombre =nombreCompleto + "***";
+            nombre = nombreCompleto ;
         }
         else{
             nombre = nombreCompleto;
         }
         if(numeroMatriculaAlumno.length() < 4){
             System.out.println("!ERROR¡ Nº de matricula muy corto");
-            numeroMatricula = numeroMatriculaAlumno + "****";
+            numeroMatricula = numeroMatriculaAlumno;
         }
         else{
             numeroMatricula = numeroMatriculaAlumno;
@@ -34,34 +34,53 @@ public class Alumno {
     public String getNombre() {
         return nombre;
     }
-    
+
     /**
      * Cambia el nombre del alumno
      */
     public void cambiarnombre(String nuevoNombre) {
         nombre = nuevoNombre;
     }
-    
+
     /**
      * Imprime por pantalla los detalles del alumno
      */
     public void imprimeDetalles() {
         System.out.println(nombre + " (" + numeroMatricula + ") - " + edad + " aÃ±os");
     }
-    
+
     /**
      * Devuelve el nombre de usuario que el alumno debe configurar
      * en la cuenta de Github en formato de 7 caracateres
      */
     public void getNombreUsuarioGithub() {
-        /*if (nombre.length() < 3){
+        int error1 = 0;
+        int error2 = 0;
+        if (nombre.length() < 3){
             System.out.println("ERROR Usuario muy corto");
+            error1 = 1;
         }
-        else if (numeroMatricula.length() < 4){
+        
+        if (numeroMatricula.length() < 4){
             System.out.println("ERROR Matricula muy corta");
+            error2 = 1;
         }
-        else{*/
-            System.out.println(nombre.substring(0, 3) + numeroMatricula.substring(0, 4));
-        //}
+        
+        if(error1 == 1){
+            if(error2 == 0){
+                System.out.println(nombre.substring(0, nombre.length()) + numeroMatricula.substring(0, 4));
+            }
+            if(error2 == 1){
+                System.out.println(nombre.substring(0, nombre.length()) + numeroMatricula.substring(0, numeroMatricula.length()));
+            }
+        }
+        else{
+            if(error2 == 0){
+                System.out.println(nombre.substring(0, 3) + numeroMatricula.substring(0, 4));
+            }
+            if(error2 == 1){
+                System.out.println(nombre.substring(0, 3) + numeroMatricula.substring(0, numeroMatricula.length()));
+            }
+        }
     }
 }
